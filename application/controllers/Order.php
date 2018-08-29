b89bedf7e2a1e598816872c47914115bf4972742
bagusy <42733293+bagusy@users.noreply.github.com>
2018-08-29 17:40:09 +0700

    update order

---
+++ application/controllers/Order.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+* 
+*/
+class Order extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+	public function index()
+	{
+		$this->load->model('order_model');
+		$data = array();
+		$q = $this->db->get('orders');
+		$data['order'] = $q->result_array();
+
+		$this->load->view('front/header', $data);
+		$this->load->view('front/order', $data);
+		$this->load->view('front/footer', $data);
+	}
+	public function add()
+	{
+		$this->load->model('order_model');
+		$data= array();
+		// $this->load->view('front/header',$data);
+		$this->load->view('front/add_order',$data);
+		// $this->load->view('front/footer',$data);
+	}
+
+	public function action_add()
+	{
+		$data= array(
+			'id_order'=>$this->input->post('id_order'),
+			'date'=>$this->input->post('date'),
+			'userid'=>$this->input->post('userid'),
+			'produkid'=>$this->input->post('produkid'),
+			'total_produk'=>$this->input->post('total_produk'),
+			'biaya'=>$this->input->post('biaya'),
+			'status'=>$this->input->post('status'),
+			'inv_num'=>$this->input->post('inv_num'),
+			'pembayaran'=>$this->input->post('pembayaran'),
+			
+		);
+		$this->db->insert('orders',$data);
+		redirect('order','refresh');
+
+	}
+
+}
--- application/views/produk.php
+++ application/views/produk.php
@@ -2,58 +2,68 @@
 			<div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop m-page__container m-body">
 				<div class="m-grid__item m-grid__item--fluid m-wrapper">
 
+								<div class="m-portlet">
+									<div class="m-portlet__head">
+										<div class="m-portlet__head-caption">
+											<div class="m-portlet__head-title">
+												<h3 class="m-portlet__head-text">
+													<a href="produk/add" class="btn btn-success">Tambah Poduk</a>
+												</h3>
+											</div>
+										</div>
+									</div>
+									<div class="m-portlet__body">
+										<div class="m-section">
+											<div class="m-section__content">
+												<table class="table table-striped m-table">
+													<thead>
+														<th>id</th>
+														<th>tanggal</th>
+														<th>name</th>
+														<th>kategori</th>
+														<th>sku</th>
+														<th>gambar</th>
+														<th>berat </th>
+														<th>stok</th>
+														<th>url</th>
+														<th>harga beli</th>
+														<th>harga jual</th>
+														<th>harga diskon</th>
+														<th>asal</th>
+														<th>action</th>
+													</thead>
+													<tbody>														
+														<?php foreach($produk as $p ){ ?>
+														<tr>
+															<td><?=$p['id_produk'];?></td>
+															<td><?=$p['date'];?></td>
+															<td><?=$p['name'];?></td>
+															<td><?=$p['kategori'];?></td>
+															<td><?=$p['sku'];?></td>
+															<td><img src="<?=$p['gambar_produk'];?>" width="40px"></td>
+															<td><?=$p['berat'];?></td>
+															<td><?=$p['stok'];?></td>
+															<td><a href="<?=$p['url_drop'];?>" target='_new' data-toggle="m-tooltip" data-placement="bottom" title="" data-original-title="<?=$p['url_drop'];?>"><i class="fa fa-link"></i></td>
+															<td><?=$p['harga_drop'];?></td>
+															<td><?=$p['harga'];?></td>
+															<td><?=$p['harga2'];?></td>
+															<td><?=$p['asal_pengiriman'];?></td>
+															<td>
+																<a href="produk/update/<?=$p['id_produk'];?>" class="btn btn-sm btn-outline-success">update</a> 
+																<a href="produk/delete/<?=$p['id_produk'];?>" class="btn btn-sm btn-outline-danger" data-toggle="modal">delete</a>
+															</td>
+														</tr>
+														<?php } ?>
+													</tbody>
+												</table>
+											</div>
+										</div>
 
-<a href="produk/add" class="btn btn-success">Tambah Poduk</a>
-<table border="1" class="table">
-	<thead>
-		<th>id produk</th>
-		<th>date</th>
-		<th>name</th>
-		<th>deskripsi</th>
-		<th>kategori</th>
-		<th>sku</th>
-		<th>gambar</th>
-		<th>berat </th>
-		<th>stok</th>
-		<th>url</th>
-		<th>harga drop</th>
-		<th>harga 1</th>
-		<th>harga 2</th>
-		<th>asal</th>
-		<th>action</th>
-	</thead>
-	<tbody>
-		<?php foreach($produk as $p ){ ?>
-		<tr>
-			<td><?=$p['id_produk'];?></td>
-			<td><?=$p['date'];?></td>
-			<td><?=$p['name'];?></td>
-			<td><?=$p['deskripsi'];?></td>
-			<td><?=$p['kategori'];?></td>
-			<td><?=$p['sku'];?></td>
-			<td><img src="<?=$p['gambar_produk'];?>" width="40px"></td>
-			<td><?=$p['berat'];?></td>
-			<td><?=$p['stok'];?></td>
-			<td><?=$p['url_drop'];?></td>
-			<td><?=$p['harga_drop'];?></td>
-			<td><?=$p['harga'];?></td>
-			<td><?=$p['harga2'];?></td>
-			<td><?=$p['asal_pengiriman'];?></td>
-			<td>
-				<a href="produk/update/<?=$p['id_produk'];?>">update</a> 
-				<a href="produk/delete/<?=$p['id_produk'];?>">delete</a>
-			</td>
-		</tr>
-		<?php } ?>
-	</tbody>
-</table>
+										<!--end::Section-->
+									</div>
 
-<pre>
-	<?php
-print_r($produk);
-	?>
-
-</pre>
+									<!--end::Form-->
+								</div>
 
 				</div>
 			</div>
