fa9f13c9dc54f7f9298d62ec32b1cb79b0cd6a1f
bagusy <bagusyy11@gmail.com>
2018-08-31 12:48:26 +0700

    update

--- application/views/dasbor.php
+++ application/views/dasbor.php
@@ -53,7 +53,6 @@
 								 	<i class="fa fa-area-chart"></i>
 								  		Omset 14 hari terahir
 								</div> 
-
 								<div class="card-body">
 									<div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
 										<div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
