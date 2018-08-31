5694751aaa128404bb73b5af363882d0d08d29ad
bagusy <42733293+bagusy@users.noreply.github.com>
2018-08-31 12:52:51 +0700

    baru

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -17,10 +17,24 @@ class Order extends CI_Controller
 		$q = $this->db->get('orders');
 		$data['order'] = $q->result_array();
 
+		
+
 		$this->load->view('front/header', $data);
 		$this->load->view('front/order', $data);
 		$this->load->view('front/footer', $data);
 	}
+
+	public function item()
+	{
+		$this->load->model('produk_model');
+		$id = $this->uri->segment(2);
+		$data['produk'] = $this->produk_model->getId($id);
+		$this->load->view('front/header',$data);
+		$this->load->view('front/add_order',$data);
+		$this->load->view('front/footer',$data);
+
+	}
+
 	public function add()
 	{
 		$this->load->model('order_model');
@@ -33,10 +47,10 @@ class Order extends CI_Controller
 	public function action_add()
 	{
 		$data= array(
-			'id_order'=>$this->input->post('id_order'),
-			'date'=>$this->input->post('date'),
-			'userid'=>$this->input->post('userid'),
-			'produkid'=>$this->input->post('produkid'),
+			'id_order'=>'',
+			'date'=>date('Y-m-d H:i:s'),
+			'userid'=>'',
+			'produkid'=>'',
 			'total_produk'=>$this->input->post('total_produk'),
 			'biaya'=>$this->input->post('biaya'),
 			'status'=>$this->input->post('status'),
@@ -45,7 +59,7 @@ class Order extends CI_Controller
 			
 		);
 		$this->db->insert('orders',$data);
-		redirect('order','refresh');
+		redirect('front/order','refresh');
 
 	}
 
--- application/models/produk_model.php
+++ application/models/produk_model.php
@@ -5,9 +5,18 @@
  */
 class Produk_model extends CI_Model{
 	
-	function produk_model()
+	function __construct()
 	{
 		parent::__construct();
+	}
+
+	public function getId($id)
+	{
+		echo $id;
+		$this->db->where('id_produk', $id);
+		$item = $this->db->get('produk');
+		
+		return $item->result_array();
 	}
 	
 }
